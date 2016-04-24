7d5fbb8baae97c696540bed2d720e5c4bd2d832b
Jasper Wong <766453486@qq.com>
2016-04-24 23:27:01 +0800

    change the framework

--- User/stm32f10x_it.c
+++ User/stm32f10x_it.c
@@ -29,7 +29,7 @@
 #include "bsp_pwm_output.h"
 #include <math.h>
 
-#define RECEIVE_BUF_SIZE 32
+
 
 extern volatile u32 time;
 extern uint32_t time_index;
@@ -170,98 +170,13 @@ void TIM2_IRQHandler(void)
     }
 }
 
+extern void OnRecieve(int16_t data);
+
 void USART1_IRQHandler(void)
 {
-    static int8_t roll_buffer[10];
-    static int8_t yaw_buffer[10];
-    static uint8_t buffer_bit=0;
-    static uint8_t bit=0;
-    static uint8_t isDone=0;
-    int16_t roll;
-    int16_t yaw;
-    uint8_t read_bit;
-    int16_t roll_final=0;
-    int16_t yaw_final=0;
-    int8_t temp;
-    int16_t data[RECEIVE_BUF_SIZE];
-    uint8_t isMinus_roll=0;
-    uint8_t isMinus_yaw=0;
-		
-    if ((!isDone)&&(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET))
-    {
-        temp= USART_ReceiveData(USART1);
-        if (temp=='X')   bit=0;
-        if (temp=='\n')  isDone=1;
-        data[bit]=temp;
-        bit++;
-    }
-
-    if (isDone)
-    {
-        roll=0;
-        yaw=0;
-        isMinus_roll=0;
-        isMinus_yaw=0;
-        read_bit=2;
-			
-        /**getRoll**/
-        while (data[read_bit]!=' ')
-        {
-            if (data[read_bit]=='-')
-            {
-                isMinus_roll=1;
-                read_bit++;
-                continue;
-            }
-            roll=roll*10+(data[read_bit]-'0');
-            read_bit++;
-        }
-        read_bit+=3;
-				
-        /**getyaw**/
-        while (data[read_bit]!=' ')
-        {
-            if (data[read_bit]=='-')
-            {
-                isMinus_yaw=1;
-                read_bit++;
-                continue;
-            }
-            yaw=yaw*10+(data[read_bit]-'0');
-//            read_counts++;
-            read_bit++;
-        }
-//        printf("%c%c\n",data[2],data[]);
-        if (isMinus_roll)
-        {
-            roll=-roll;
-            isMinus_roll=0;
-        }
-        if (isMinus_yaw)
-        {
-            yaw=-yaw;
-            isMinus_yaw=0;
-        }
-        roll+=90;
-
-        roll_buffer[buffer_bit]=roll;
-        yaw_buffer[buffer_bit]=yaw;
-				buffer_bit++;
-				if(buffer_bit>=11) buffer_bit=0;
-				
-				for(int i=0;i<10;i++)
-				{
-					roll_final+=roll_buffer[i];
-					yaw_final+=yaw_buffer[i];
-				}
-				roll_final/=10;
-				yaw_final/=10;
-				
-        printf("%c%c",roll_final,yaw_final);
-        Roll(roll);
-        YawToAngle(yaw);
-        if (roll==0) Reset();
-        isDone=0;
+    if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+    {		
+		OnRecieve(USART_ReceiveData(USART1));
     }
 }
 /**
